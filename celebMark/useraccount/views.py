edbc178c415a6d840b444499c26ef7f2732fb18e
Abhi <abhi.chaurasia204@gmail.com>
2019-02-03 16:51:15 +0530

    create user registration API view

--- celebMark/useraccount/views.py
+++ celebMark/useraccount/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from rest_framework import permissions
+from rest_framework.generics import CreateAPIView
+from useraccount.models import User
+from .serializers import UserSerializer
 # Create your views here.
+
+class register_user(CreateAPIView):
+    
+    model = User
+    serializer_class = UserSerializer
+    
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+
