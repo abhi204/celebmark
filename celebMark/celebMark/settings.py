3f5773fbf36411cdf961d775569441f2bff25f5d
Abhi <abhi.chaurasia204@gmail.com>
2019-02-03 19:26:37 +0530

    Add token auth in settings.py

--- celebMark/celebMark/settings.py
+++ celebMark/celebMark/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'useraccount',
 ]
 
@@ -140,6 +141,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'mystatic/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
