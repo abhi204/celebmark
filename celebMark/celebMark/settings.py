cc839c7edbe32f4d92661e53d4d2ec544bf7922d
Abhi <abhi.chaurasia204@gmail.com>
2019-02-03 21:00:04 +0530

    install coreapi and djangorestframework-simplejwt

--- celebMark/celebMark/settings.py
+++ celebMark/celebMark/settings.py
@@ -142,7 +142,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':(
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES':(
         'rest_framework.permissions.IsAuthenticated',
