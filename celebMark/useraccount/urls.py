2486ada7401b3e0e521ac12d8014f18380e0f65d
Abhi <abhi.chaurasia204@gmail.com>
2019-02-03 20:16:05 +0530

    Add auth-token to urlpatterns

--- celebMark/useraccount/urls.py
+++ celebMark/useraccount/urls.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^register/$',views.register_user.as_view(), name='register-user')
+    re_path(r'^register/$',views.register_user.as_view(), name='register-user'),
+    re_path(r'^token-auth/',views.CustomAuthToken.as_view(), name='get-auth-token')
 ]
