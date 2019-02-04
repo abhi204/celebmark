20916bbe94d2d4ef96e710bd43b6c0cbeb694197
Abhi <abhi.chaurasia204@gmail.com>
2019-02-04 10:10:52 +0530

    remove auth classes other than jwt

--- celebMark/celebMark/settings.py
+++ celebMark/celebMark/settings.py
@@ -142,8 +142,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':(
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES':(
