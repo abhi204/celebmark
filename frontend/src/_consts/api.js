f3ff8f3996469a8c2edb0cc3e2a4a633a80c95d9
Abhi <abhi.chaurasia204@gmail.com>
2019-02-07 00:56:19 +0530

    create auth related constants and api endpoints

---
+++ frontend/src/_consts/api.js
@@ -0,0 +1,5 @@
+// All API endpoints defined here
+
+export const API_HOST = 'http://localhost:8000';
+export const API_GET_TOKEN = '/auth/token/obtain/';
+export const API_REFRESH_TOKEN = '/auth/token/refresh/';
---
+++ frontend/src/_consts/auth.js
@@ -0,0 +1,10 @@
+export const LOGIN_REQUEST = "Request Login";
+export const LOGIN_SUCESS = "Login Successful";
+export const LOGIN_FAILED = "Login Failed";
+export const LOGIN_ERROR = "Login Error";
+export const LOGOUT = "Logged Out";
+
+export const NO_COOKIES = "Cookies disabled";
+export const LOGIN_REDIRECT = "Redirect Login";
+//AccessToken, RefreshToken name inside cookie => access, refresh
+
