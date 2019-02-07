ae1cbe50b02139c79bb498b496d6d3aa09720e34
Abhi <abhi.chaurasia204@gmail.com>
2019-02-07 19:29:24 +0530

    Create loggedIn reducer

--- frontend/src/_reducers/index.js
+++ frontend/src/_reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-
 import { connectRouter } from 'connected-react-router';
+import loggedInReducer from './logged_in_reducer';
 
 export default (history) => combineReducers({
     router: connectRouter(history), //saves the router state
-
+    loggedIn: loggedInReducer,
 })
