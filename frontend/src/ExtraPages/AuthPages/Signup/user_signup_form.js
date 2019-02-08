107833646c6ea9d69244fc6fb1d9d96c5701d288
Abhi <abhi.chaurasia204@gmail.com>
2019-02-08 23:04:55 +0530

    set agreeTerms checkbox and minor changes in user signup form

--- frontend/src/ExtraPages/AuthPages/Signup/user_signup_form.js
+++ frontend/src/ExtraPages/AuthPages/Signup/user_signup_form.js
@@ -79,60 +79,8 @@ class UserSignupForm extends Component {
 
 UserSignupForm = reduxForm({
     form: 'user_signup_form', //unique name
-    // validate: [required],
     pristine: false,
     fields: fields.map(field => (field.name))
 })(UserSignupForm);
 
 export default UserSignupForm;
-
-/*
-<Form>
-                <Form.Group widths='equal'>
-                    <Form.Field>
-                        <label>First Name</label>
-                        <input placeholder='First Name' type='text' />
-                    </Form.Field>
-                    <Form.Field>
-                        <label>Last Name</label>
-                        <input placeholder='Last Name' type='text' />
-                    </Form.Field>
-                    <Form.Field>
-                        <label>Username</label>
-                        <input placeholder='Username' type='text' />
-                    </Form.Field>
-                </Form.Group>
-                <Form.Group widths='equal'>
-                    <Form.Field>
-                        <label>Email Address</label>
-                                <input placeholder='Your Email' type='email' />
-                            </Form.Field>
-                            <Form.Field>
-                                <label>Phone Number</label>
-                                <input placeholder='Phone' type='tel'/>
-                            </Form.Field>
-                        </Form.Group>
-
-                        <Form.Group widths='equal'>
-                        <Form.Field>
-                        <label>Password</label>
-                            <input placeholder='Password' type='password' />
-                        </Form.Field>
-                        <Form.Field>
-                        <label>Confirm Password</label>
-                            <input placeholder='Re-Enter Password' type='password' />
-                        </Form.Field>
-
-
-                        <Form.Field>
-                        <label>Upload Profile Picture</label>
-                            <input type="file" name="pic" accept="image/*" />
-                        </Form.Field>
-                        </Form.Group>
-
-                        <Form.Field>
-                        <Checkbox label='I agree to the Terms and Conditions' />
-                        </Form.Field>
-                            <Button type='submit' color='green' >Signup</Button>
-                        </Form>
-*/
