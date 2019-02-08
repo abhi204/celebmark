95b4e1e0d76f2533163b8616e3d8a79d56bfc489
Abhi <abhi.chaurasia204@gmail.com>
2019-02-08 13:32:19 +0530

    Setup Redux-form

--- frontend/src/_reducers/index.js
+++ frontend/src/_reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import loggedInReducer from './logged_in_reducer';
+import { reducer as formReducer } from 'redux-form';
 
 export default (history) => combineReducers({
     router: connectRouter(history), //saves the router state
+    form: formReducer, //Redux-Form
     loggedIn: loggedInReducer,
 })
