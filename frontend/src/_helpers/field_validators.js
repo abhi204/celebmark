bf173954940a75620769803a672e5889b4012ccd
Abhi <abhi.chaurasia204@gmail.com>
2019-02-08 23:09:28 +0530

    (Minor) isMobileNumber text update

--- frontend/src/_helpers/field_validators.js
+++ frontend/src/_helpers/field_validators.js
@@ -9,4 +9,4 @@ export const matchPassword = (value) => (storedPassword === value ? undefined: "
 export const isAlpha = value => ( /^[A-Za-z]+$/i.test(value) ? undefined: "Enter Valid Characters")
 export const isNum = value => ( /^[0-9]+$/i.test(value) ? undefined: "Enter Digits Only")
 export const isAlphaNum = value => ( /^[A-Za-z0-9]+$/i.test(value) ? undefined: "No Special Characters or whitespace allowed")
-export const isMobileNumber = value => (value && value.length === 10 && isNum(value)===undefined ? undefined: "Enter a valid Mobile number (Excluding Country Code)");
+export const isMobileNumber = value => (value && value.length === 10 && isNum(value)===undefined ? undefined: "Enter a valid Mobile number (without country Code)");
