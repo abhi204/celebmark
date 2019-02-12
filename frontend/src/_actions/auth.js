ae44d92ae83836a6fe1c0e1eaa09b2c1a04a364d
Abhi <abhi.chaurasia204@gmail.com>
2019-02-12 20:02:14 +0530

    (1A)Update auth action creator (root action creator) to async function

--- frontend/src/_actions/auth.js
+++ frontend/src/_actions/auth.js
@@ -8,8 +8,8 @@ import sendCredentials from '../_helpers/api_login';
 
 // After login attempt, reducer will recieve payload which tells if the login was sucessful
 // (i.e. refresh, access tokens were set in cookie)
-export const login = (user_name, password) => {
-    let loginAttempt = sendCredentials(user_name, password);
+export const login = async (user_name, password) => {
+    let loginAttempt = await sendCredentials(user_name, password);
     if(loginAttempt==='OK')
         return {type: LOGIN_SUCESS, payload: { user_name } }
     else
