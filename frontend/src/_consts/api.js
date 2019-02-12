287ae3aeaa8645f0b667e2cde19e4687e4012c24
Abhi <abhi.chaurasia204@gmail.com>
2019-02-12 19:58:40 +0530

    (1A)Update Constant Names

--- frontend/src/_consts/api.js
+++ frontend/src/_consts/api.js
@@ -1,5 +1,6 @@
 // All API endpoints defined here
 
 export const API_HOST = 'http://localhost:8000';
-export const API_GET_TOKEN = '/auth/token/obtain/';
-export const API_REFRESH_TOKEN = '/auth/token/refresh/';
+export const API_GET_TOKEN = `${API_HOST}/auth/token/obtain/`;
+export const API_REFRESH_TOKEN = `${API_HOST}/auth/token/refresh/`;
+export const API_USER_REGISTER = `${API_HOST}/users/register/`;
--- frontend/src/_consts/auth.js
+++ frontend/src/_consts/auth.js
@@ -5,7 +5,7 @@ export const REFRESH_TOKEN_EXPIRE = 7;     // 7 days
 export const NO_COOKIES = "Cookies disabled";
 
 export const LOGIN_REQUEST = "Request Login";
-export const LOGIN_SUCESS = "Login Successful";
+export const LOGIN_SUCCESS = "Login Successful";
 export const LOGIN_FAILED = "Login Failed";
 export const LOGIN_ERROR = "Login Error";
 export const LOGOUT = "Logged Out";
