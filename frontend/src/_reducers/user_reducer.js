65f2c63a1d8ac5343adbddca8b71e0e5f40077ca
Abhi <abhi.chaurasia204@gmail.com>
2019-02-13 18:47:06 +0530

    on succesful signin -> user details are in reducer loggedIn as loggedIn.details

--- frontend/src/_reducers/user_reducer.js
+++ frontend/src/_reducers/user_reducer.js
@@ -1,4 +1,4 @@
-import { LOGIN_SUCCESS, LOGIN_ERROR, LOGIN_FAILED } from "../_consts/auth";
+import { LOGIN_SUCCESS, LOGIN_ERROR, LOGIN_FAILED, LOGOUT } from "../_consts/auth";
 
 let initialState = { loggedIn: false, details: {} };
 
@@ -6,11 +6,12 @@ let userReducer = (state=initialState, action) => {
     switch (action.type) {
         case LOGIN_SUCCESS:
             console.log("LOGIN reducer is true")
-            return { ...state, loggedIn: true};
+            return { ...state, loggedIn: true, details: action.payload };
+        case LOGOUT:
         case LOGIN_ERROR:
         case LOGIN_FAILED:
             console.log("LOGIN reducer is false")
-            return { ...state, loggedIn: false};
+            return initialState;
         default:
             return state
     }
