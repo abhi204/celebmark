54293ec5c6436a069eebe4587eddaa663e5e7e1a
Abhi <abhi.chaurasia204@gmail.com>
2019-02-13 18:18:35 +0530

    SendCredetials (root helper) now returns server response object

--- frontend/src/_helpers/api_login.js
+++ frontend/src/_helpers/api_login.js
@@ -3,28 +3,33 @@ import { API_GET_TOKEN } from "../_consts/api";
 import { ACCESS_TOKEN_EXPIRE, REFRESH_TOKEN_EXPIRE } from "../_consts/auth";
 import { setCookie } from "./cookies";
 
+/*
+ IF successful sendCredentials saves tokens to cookies
+ and sends the server response which is in the format:
+ JSON Object -> {
+    'access': ..., 
+    'refresh': ...,
+    'user': ...,
+    }
+ * 
+ *
+ IF failed, returns the error response object from server 
+ */
 async function sendCredentials(user_name,password){
+    const loginData = { user_name, password };
     let response = {};
-    const data = { user_name, password };
-    await axios.post(API_GET_TOKEN,data)
-        .then(resp => {
-            if(resp.status >=200 && resp.status < 300){
-                response = resp.data
-                console.log("SUCCESS REPONSE", resp)
-            }
-            else
-                throw resp
-        })
-        .catch(err => {
-            response = err;
-        })
+    await axios.post(API_GET_TOKEN, loginData)
+        .then(resp => { response = resp.data })
+        .catch(err => { response = err.response.data })
 
-    if(!(response.access && response.refresh))
-        return response;
-
-    setCookie('access', response.access, ACCESS_TOKEN_EXPIRE);
-    setCookie('refresh', response.refresh, REFRESH_TOKEN_EXPIRE);
-    return 'OK';    
+    if(response.access && response.refresh){
+        setCookie('access', response.access, ACCESS_TOKEN_EXPIRE);
+        setCookie('refresh', response.refresh, REFRESH_TOKEN_EXPIRE);
+        console.log("SET COOKIES COMPLETE")
+        //dummy success response user data
+        response.user = {name: "ABHI CHAURASIA", dummy:"TRUE", user_name:"abhi"}
+    }
+    return response;
 }
 
 export default sendCredentials;
