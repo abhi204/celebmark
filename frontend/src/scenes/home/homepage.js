5be893f8863c059abf3ed2a62d61e507dfe77f90
Abhi <abhi.chaurasia204@gmail.com>
2019-02-19 13:12:10 +0530

    remove PublicHomepage login

--- frontend/src/scenes/home/homepage.js
+++ frontend/src/scenes/home/homepage.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import Footer from '_components/Footer/footer';
 import UserDashboard from '../userdashboard/loggedin';
-import BeforeLoginNav from '_components/navigation/beforelogin/beforeloginnav';
-
 
 export default class PublicHomePage extends Component {
     render(){
         return (
           <div>
-
-            <BeforeLoginNav/>
 
             <UserDashboard/>
 
