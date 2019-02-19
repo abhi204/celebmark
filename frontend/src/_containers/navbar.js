024f53813c1e40a0613a2e83e1654b75127337d8
dntandan <deepnarayan006@gmail.com>
2019-02-19 12:41:43 +0530

    mobile navbar design completed + temp favicon added

--- frontend/src/_containers/navbar.js
+++ frontend/src/_containers/navbar.js
@@ -21,13 +21,13 @@ class NavBar extends Component {
     render(){
         const { user_name } = this.props.user
         return (
-                <div >
-                  <MDBRow>
+              <div >
+                <MDBRow>
                       <MDBNavbar color="white" light className="d-none d-sm-flex pt-1" scrolling fixed="top">
                         <MDBCol size="1"></MDBCol>
 
                         <MDBCol size="1">
-                          <MDBNavItem fluid className="d-inline  my-auto">
+                          <MDBNavItem fluid='true' className="d-inline  my-auto">
                             <img src={favIcon}  height="50" alt="" />
                           </MDBNavItem>
                         </MDBCol>
@@ -73,32 +73,33 @@ class NavBar extends Component {
                 </MDBRow>
 
                 {/* Navabar for Mobile Devices */}
-                <MDBNavbar style={this.state.color} className="d-flex d-sm-none p-0 z-depth-5" fixed="top">
-                <MDBNavbarNav className="d-inline-flex flex-row flex-fill">
+                <MDBNavbar style={this.state.color} className="d-flex d-sm-none p-0 z-depth-1" fixed="top">
+                  <MDBNavbarNav className="d-inline-flex flex-row flex-fill">
                     <MDBNavItem className="my-auto" >
                         <MDBNavbarBrand >
-                            <img src={smallIcon} style={{height: "2em", padding: 0}}/>
+                            <img src={favIcon} style={{height: "2em", padding: 0}}/>
                         </MDBNavbarBrand>
                     </MDBNavItem >
                     <MDBNavItem  className="my-auto">
                         <MDBNavLink to="/">
-                            <MDBIcon icon="search" className="green-text" size="2x"/>
+                            <MDBIcon fab icon="superpowers" className="black-text" size="2x"/>
                         </MDBNavLink>
                     </MDBNavItem>
                     <MDBNavItem  className="my-auto">
                         <MDBNavLink to="/">
-                            <MDBIcon icon="envelope" className="green-text" size="2x"/>
+                            <MDBIcon far icon="envelope-open" className="black-text" size="2x"/>
                         </MDBNavLink>
                     </MDBNavItem>
                     <MDBNavItem  className="my-auto">
                         <MDBNavLink to="/">
-                        <img src={userImage} className="rounded-circle z-depth-0"
-                                style={{height: "3em", padding: 0}} />
+                          <img src={userImage} className="rounded-circle z-depth-0"
+                                style={{height: "2.4em", padding: 0}} />
                         </MDBNavLink>
                     </MDBNavItem>
-                </MDBNavbarNav>
+                  </MDBNavbarNav>
                 </MDBNavbar>
-                </div>)
+                
+              </div>)
     }
 }
 
