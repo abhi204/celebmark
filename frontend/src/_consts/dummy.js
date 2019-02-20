fee1b3a68001beebca224d6dab5f719d83db67c6
Abhi <abhi.chaurasia204@gmail.com>
2019-02-20 19:31:29 +0530

    Add sample image url for sponser list

--- frontend/src/_consts/dummy.js
+++ frontend/src/_consts/dummy.js
@@ -2,4 +2,5 @@ export const userImage = "https://react.semantic-ui.com/images/avatar/large/matt
 export const favIcon =  "https://i.ibb.co/nMYYxtd/celeblogo.png";
 export const smallIcon = "https://i.ibb.co/rdjYVnq/cm-star-ico.png";
 
-export const carouselUrl = "https://source.unsplash.com/featured/?concert"; // dummy carousel image endpoint
+export const carouselUrl = "https://source.unsplash.com/featured/?concert";
+export const sponsorUrl = "https://source.unsplash.com/160x80/?company,logo";
