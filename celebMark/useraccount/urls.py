2cc3b3ee84107e0ffc162f6400509f4e246a322f
Abhi <abhi.chaurasia204@gmail.com>
2019-03-10 16:38:51 +0530

    Add basic user search functionality in backend

--- celebMark/useraccount/urls.py
+++ celebMark/useraccount/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('register/',views.register_user.as_view(), name='register-user'),
+    path('register/', views.register_user.as_view(), name='register-user'),
+    re_path('^search/?', views.CelebListView.as_view()),
 ]
--- celebMark/useraccount/views.py
+++ celebMark/useraccount/views.py
@@ -1,7 +1,8 @@
 from rest_framework import permissions
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
 from useraccount.models import User
 from .serializers import UserSerializer
+from rest_framework import filters
 
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import CustomTokenObtainPairSerializer
@@ -18,3 +19,11 @@ class register_user(CreateAPIView):
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+# View to Filter and return list of celebs
+class CelebListView(ListAPIView):
+    queryset = User.objects.all() # change to celeb user model
+    serializer_class = UserSerializer # change this to a new serializer for celeb
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=user_name', 'first_name', 'last_name')
+    permission_classes = [permissions.AllowAny,]
