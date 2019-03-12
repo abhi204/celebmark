d8533e58f1ca98b070bc48bc51cad225e33d88a3
dntandan <deepnarayan006@gmail.com>
2019-03-12 19:20:42 +0530

    gridding and ui completed for search page

--- frontend/src/App/app_scenes/search/search.js
+++ frontend/src/App/app_scenes/search/search.js
@@ -2,30 +2,34 @@ import React, { Component } from 'react';
 import { MDBSticky, MDBStickyContent, MDBContainer, MDBRow, MDBCol, MDBCardTitle } from 'mdbreact';
 import FilterSection from './containers/filter_section/filter_section';
 import ResultSection from './containers/result_section/result_section';
+import TagsSection from './containers/filter_section/tags_section';
+import SortSection from './containers/filter_section/sort_section';
 
 class Search extends Component {
 
     render(){
         return (
-            <div><center>
-            <MDBRow className="mt-4 p-3" />
-                <MDBRow className="ml-3 mr-3">
-                    <MDBCol>
-                        <div style={{position: "fixed"}}>
-                        <h3><strong>Search Results</strong></h3>
-                        <hr/> 
-                        <FilterSection/>
-                        </div>
-                    </MDBCol>
-                    <MDBCol size="8">
-                        <ResultSection />
-                        <ResultSection />
-                        <ResultSection />
-                        <ResultSection />
-                        <ResultSection />
-                    </MDBCol>
-                </MDBRow>
-            </center></div>
+            <MDBContainer fluid>
+            <MDBRow className="mt-3 p-3"/>
+            <MDBRow className="mt-3 ml-5">
+                <MDBCol size="3" className="ml-5 ">
+                    <div >
+                    <FilterSection/>
+                    <SortSection/>
+                    <TagsSection/>
+                    </div>
+                </MDBCol>
+                <MDBCol size="8"  className="ml-1 mt-1">
+                    <ResultSection />
+                    <ResultSection />
+                    <ResultSection />
+                    <ResultSection />
+                    <ResultSection />
+                    <ResultSection />
+                    <ResultSection />
+                </MDBCol>
+            </MDBRow>
+            </MDBContainer>
         );
     }
 }
