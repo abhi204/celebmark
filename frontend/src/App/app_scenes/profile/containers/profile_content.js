80eec856f981c7e88bb5a123eeaa9d735e2f5707
dntandan <deepnarayan006@gmail.com>
2019-04-20 19:11:21 +0530

    profile design first iteration

--- frontend/src/App/app_scenes/profile/containers/profile_content.js
+++ frontend/src/App/app_scenes/profile/containers/profile_content.js
@@ -1,74 +1,96 @@
-import React, { Component } from "react";
-import { connect } from 'react-redux';
-import { MDBTabPane, MDBTabContent, MDBNav, MDBNavItem, MDBNavLink, MDBBtn, MDBNavbarNav, MDBIcon } from "mdbreact";
+import React, {Component} from "react";
+import {connect} from 'react-redux';
+import {
+    MDBTabPane,
+    MDBTabContent,
+    MDBNav,
+    MDBNavItem,
+    MDBNavLink,
+    MDBBtn,
+    MDBNavbarNav,
+    MDBIcon,
+    MDBContainer,
+    MDBRow,
+    MDBCOL,
+} from "mdbreact";
 import AboutTab from './about'
 import GalleryTab from './gallery/gallery'
 import './profile_content.css';
 
 class TabsPage extends Component {
-state = {
-  activeItemClassicTabs1: "1",
-}
+    state = {
+        activeItemClassicTabs1: "1",
+    }
 
-toggleClassicTabs1 = tab => () => {
-  if (this.state.activeItemClassicTabs1 !== tab) {
-  this.setState({
-    activeItemClassicTabs1: tab
-  });
-  }
-}
+    toggleClassicTabs1 = tab => () => {
+        if (this.state.activeItemClassicTabs1 !== tab) {
+            this.setState({
+                activeItemClassicTabs1: tab
+            });
+        }
+    }
 
-render() {
-  return (
-        <div className="classic-tabs">
-          <MDBNav className="profile-nav-tabs pl-2" classicTabs>
-            <MDBNavItem>
-              <MDBNavLink to="#" className={this.state.activeItemClassicTabs1==="1" ? "active tab-header" : " tab-header " } onClick={this.toggleClassicTabs1("1")}>
-                <span style={{ fontSize: "large", fontWeight:"normal", color:"black" }}>About</span>
-              </MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem hidden={this.props.profile.gallery.length===0}>
-              <MDBNavLink to="#" className={this.state.activeItemClassicTabs1==="2" ? "active tab-header" : " tab-header " } onClick={this.toggleClassicTabs1("2")}>
-              <span style={{ fontSize: "large", fontWeight:"normal", color:"black" }}>Gallery</span>
-              </MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="#" className={this.state.activeItemClassicTabs1==="3" ? "active tab-header" : "tab-header" } onClick={this.toggleClassicTabs1("3")}>
-              <span style={{ fontSize: "large", fontWeight:"normal", color:"black" }}>Events</span>
-              </MDBNavLink>
-            </MDBNavItem>
-            <MDBNavbarNav right className="d-flex flex-row mr-5">
-            <MDBNavItem>
-            {/* <HandlesList handles={this.props.profile.handles} /> */}
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBBtn color="black" className="invite-btn mr-5" >
-              <span style={{ fontWeight:"normal", color:"white" }}>Send Invite</span>
-              </MDBBtn>
-            </MDBNavItem>
-            <MDBNavItem className="mt-5">
-              <span style={{ fontSize: "large", fontWeight:"normal", color:"black" }}><MDBIcon far icon="star"/></span>
-            </MDBNavItem>
-          </MDBNavbarNav>
-          </MDBNav>
-          <MDBTabContent activeItem={this.state.activeItemClassicTabs1}>
-            <MDBTabPane tabId="1">
-              <AboutTab />
-            </MDBTabPane>
-            <MDBTabPane tabId="2">
-              <GalleryTab/>
-            </MDBTabPane>
-            <MDBTabPane tabId="3">
-              EVENTS
-            </MDBTabPane>
-          </MDBTabContent>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <MDBContainer>
+                <div className="classic-tabs">
+                    <MDBNav className="profile-nav-tabs pl-2" classicTabs >
+                        <MDBNavItem>
+                            <MDBNavLink to="#"
+                                        className={this.state.activeItemClassicTabs1 === "1" ? "active tab-header" : " tab-header "}
+                                        onClick={this.toggleClassicTabs1("1")}>
+                                <span style={{fontSize: "large", fontWeight: "normal", color: "black"}}>About</span>
+                            </MDBNavLink>
+                        </MDBNavItem>
+                        <MDBNavItem hidden={this.props.profile.gallery.length === 0}>
+                            <MDBNavLink to="#"
+                                        className={this.state.activeItemClassicTabs1 === "2" ? "active tab-header" : " tab-header "}
+                                        onClick={this.toggleClassicTabs1("2")}>
+                                <span style={{fontSize: "large", fontWeight: "normal", color: "black"}}>Gallery</span>
+                            </MDBNavLink>
+                        </MDBNavItem>
+                        <MDBNavItem>
+                            <MDBNavLink to="#"
+                                        className={this.state.activeItemClassicTabs1 === "3" ? "active tab-header" : "tab-header"}
+                                        onClick={this.toggleClassicTabs1("3")}>
+                                <span style={{fontSize: "large", fontWeight: "normal", color: "black"}}>Events</span>
+                            </MDBNavLink>
+                        </MDBNavItem>
+                        <MDBNavbarNav right className="d-flex flex-row mr-5">
+                            <MDBNavItem>
+                                {/* <HandlesList handles={this.props.profile.handles} /> */}
+                            </MDBNavItem>
+                            <MDBNavItem>
+                                <MDBBtn color="black" className="invite-btn mr-5" rounded>
+                                    <span style={{fontWeight: "normal", color: "white"}}>Send Invitation</span>
+                                </MDBBtn>
+                            </MDBNavItem>
+                            <MDBNavItem className="mt-5">
+                                <span style={{fontSize: "large", fontWeight: "normal", color: "black"}}><MDBIcon far
+                                                                                                                 icon="bookmark"
+                                                                                                                 size='lg'/></span>
+                            </MDBNavItem>
+                        </MDBNavbarNav>
+                    </MDBNav>
+                    <MDBTabContent activeItem={this.state.activeItemClassicTabs1}>
+                        <MDBTabPane tabId="1">
+                            <AboutTab/>
+                        </MDBTabPane>
+                        <MDBTabPane tabId="2">
+                            <GalleryTab/>
+                        </MDBTabPane>
+                        <MDBTabPane tabId="3">
+                            EVENTS
+                        </MDBTabPane>
+                    </MDBTabContent>
+                </div>
+            </MDBContainer>
+        );
+    }
 }
 
 let mapStateToProps = (state) => (
-  { profile: state.profile }
+    {profile: state.profile}
 )
 
 export default connect(mapStateToProps)(TabsPage);
--- frontend/src/App/app_scenes/profile/profile.js
+++ frontend/src/App/app_scenes/profile/profile.js
@@ -1,41 +1,41 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getCelebProfile } from '../../../_actions/profile';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {getCelebProfile} from '../../../_actions/profile';
 import ProfileTabs from './containers/profile_content';
 import './profile.css'
 
 class ProfilePage extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = { user_name: null };
+        this.state = {user_name: null};
     }
 
-    static getDerivedStateFromProps(nextProps, prevState){
-        let { user_name } = nextProps.match.params
-        if( user_name !== prevState.user_name ){
+    static getDerivedStateFromProps(nextProps, prevState) {
+        let {user_name} = nextProps.match.params
+        if (user_name !== prevState.user_name) {
             nextProps.getCeleb(user_name);
-            return { user_name }
+            return {user_name}
         }
         return null
     }
 
-    render(){
-        let { profile } = this.props;
-        if( profile.loading )
+    render() {
+        let {profile} = this.props;
+        if (profile.loading)
             return <div>LOADING</div>
-        else if( !profile.loading && !profile.user_name )
+        else if (!profile.loading && !profile.user_name)
             return <div>NO CELEB</div>
-        else 
+        else
             return (
-                <div style={{ backgroundColor: "white"}}>
-                <ProfileTabs celeb={profile} />
+                <div style={{backgroundColor: "white"}}>
+                    <ProfileTabs celeb={profile}/>
                 </div>
             );
     }
 }
 
 const mapStateToProps = state => (
-    { profile: state.profile }
+    {profile: state.profile}
 )
 
 const mapDispatchToProps = {
