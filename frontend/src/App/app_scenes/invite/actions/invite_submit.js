0d1f28a27e119f1452b8dbdf127af1de63b4f775
Abhi <abhi.chaurasia204@gmail.com>
2019-04-27 18:58:30 +0530

    Basic invite_submit functionality complete

--- frontend/src/App/app_scenes/invite/actions/invite_submit.js
+++ frontend/src/App/app_scenes/invite/actions/invite_submit.js
@@ -3,18 +3,18 @@ import { API_INVITE } from "_consts/api";
 import { SubmissionError } from 'redux-form';
 import { inviteFields } from '../forms/invite_form'
 
-const setInvite = (apiResponseData, status) => {
-    if(status === "failed")
+const setInvite = (apiResponse, okStatus) => {
+    if(okStatus)
+    {
+        window.location.assign(apiResponse.data.payURL);
+    }
+    else if(!okStatus)
         {
-            let { data, status } = apiResponseData.response;
-            let errorKeys = _.keys(data);
-            let unknownErrorsList = _.difference(errorKeys, inviteFields);
-            if(unknownErrorsList.length > 0)
-                throw new SubmissionError({ _error: "Unknown error occured", statusCode: status});
+            let { data } = apiResponse && apiResponse.response ? apiResponse.response : {}
+            if( _.difference(_.keys(data), inviteFields).length > 0)
+                throw new SubmissionError({ _error: "Unknown error occured", });
             throw new SubmissionError(data)
         }
-    if(status === "ok" )
-        console.log("ok", apiResponseData);
 }
 
 export const inviteFormSubmit = async ( values, dispatch, props ) => {
