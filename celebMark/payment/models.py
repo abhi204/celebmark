ebd39eef2142412c4426e84f2077ce4fedc6919f
Abhi <abhi.chaurasia204@gmail.com>
2019-04-27 15:27:40 +0530

    Create Payment model

--- celebMark/payment/models.py
+++ celebMark/payment/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from useraccount.models import User
+from invite.models import Invite
 
 # Create your models here.
+class Payment(models.Model):
+    STATUS_CHOICES = (
+        ('Pending', 'Pending'),
+        ('Credit', 'Credit'),
+        ('Failed', 'Failed')
+    )
+    payment_id = models.TextField(primary_key=True)
+    payment_request_id = models.TextField(unique=True)
+    mac = models.TextField()
+    purpose = models.CharField(max_length=100, default="")
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='Pending')
+
+    # Relational Fields
+    buyer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    invite = models.OneToOneField(Invite, on_delete=models.SET_NULL, null=True)
