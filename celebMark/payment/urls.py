2801150529c2b1a154712001450c97d39a3544d8
Abhi <abhi.chaurasia204@gmail.com>
2019-04-28 16:55:53 +0530

    Create basic CheckPayStatusView

--- celebMark/payment/urls.py
+++ celebMark/payment/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('hook/', views.IMHookView.as_view()),
+    path('check/', views.CheckPayStatusView.as_view()),
 ]
--- celebMark/payment/views.py
+++ celebMark/payment/views.py
@@ -1,3 +1,5 @@
+from .helpers import create_invite_payment
+from .im_api import im_api
 from .models import Payment
 from django.shortcuts import render
 from invite.models import Invite
@@ -5,6 +7,7 @@ from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from useraccount.models import BaseUser
+from useraccount.helpers import UserOnly
 
 # Create your views here.
 class IMHookView(APIView):
@@ -22,9 +25,30 @@ class IMHookView(APIView):
         # Check and pass payment to appropriate view method
         user_obj = BaseUser.objects.get(email=post_data['buyer']).user
         if post_data['purpose'] == 'invite':
-            self.invite_pay(pay_data, user_obj)
+            create_invite_payment(pay_data, user_obj)
         return Response(status=200)
 
-    def invite_pay(self, data, user):
-        invite_obj = Invite.objects.get(payment_request_id=data['payment_request_id'])
-        Payment.objects.update_or_create(**data, user=user, invite=invite_obj)
+class CheckPayStatusView(APIView):
+    permission_classes = [ permissions.IsAuthenticated, UserOnly ]
+
+    def get(self, request):
+        payment_id = request.GET['payment_id']
+        payment_request_id = request.GET['payment_request_id']
+        im_response = im_api.payment_request_payment_status(
+            payment_request_id,
+            payment_id
+            )
+        im_response = im_response['payment_request'].copy()
+        im_response.update(im_response['payment'])
+        im_response['payment_request_id'] = payment_request_id
+        im_response['payment_id'] = payment_id
+
+        pay_data = {}
+        for field in Payment._meta.get_fields():
+            if field.name in im_response:
+                pay_data.update({field.name: im_response[field.name]})
+
+        user_obj = BaseUser.objects.get(email=im_response['buyer_email']).user
+        if pay_data['purpose'] == 'invite':
+            create_invite_payment(pay_data, user_obj)
+        return Response({ 'status': pay_data['status']})
