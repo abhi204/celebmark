1df77545e5ced7569e1d525a2ed2fe953fb15400
Abhi <abhi.chaurasia204@gmail.com>
2019-04-28 13:14:31 +0530

    Register Payment model to admin.py

--- celebMark/payment/admin.py
+++ celebMark/payment/admin.py
@@ -1,3 +1,28 @@
+from .models import Payment
 from django.contrib import admin
 
 # Register your models here.
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'payment_id',
+        'payment_request_id',
+        'purpose',
+        'status',
+        'amount',
+        'fees'
+        )
+    list_filter = (
+        'purpose',
+        'status',
+        'amount',
+        )
+    fieldsets = (
+        (None, {'fields': ('purpose',)}),
+        ('Details', {'fields': ('amount','status','invite','user')}),
+    )
+    search_fields = ('user','payment_id','payment_request_id')
+    ordering=()
+    filter_horizontal = ()
+
+admin.site.register(Payment, PaymentAdmin)
