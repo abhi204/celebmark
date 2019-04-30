070c2246de1d130094529c6be6d06e0abb6bb958
Abhi <abhi.chaurasia204@gmail.com>
2019-04-30 23:13:53 +0530

    Minor Add basic subscription purchase api + (Minor) refactors in payment app's views and helpers

--- celebMark/payment/helpers.py
+++ celebMark/payment/helpers.py
@@ -1,7 +1,22 @@
+from datetime import date, timedelta
 from invite.models import Invite
 from payment.models import Payment
+from useraccount.subscription_details import subscription_details
 
 # Create Payment record For With reference to an Invite payment
-def create_invite_payment(data, user):
-    invite_obj = Invite.objects.get(payment_request_id=data['payment_request_id'])
-    Payment.objects.update_or_create(**data, user=user, invite=invite_obj)
+def create_invite_payment(data):
+    data['invite'] = Invite.objects.get(payment_request_id=data['payment_request_id'])
+    Payment.objects.update_or_create(payment_id=data['payment_id'], defaults=data)
+
+def create_subscription_payment(data):
+    Payment.objects.update_or_create(payment_id=data['payment_id'], defaults=data)
+    
+    # Purpose format => "subscribe {name of subscription}"
+    subscription_detail = subscription_details[data['purpose'].split()[1]] 
+
+    user_obj = data['user']
+    user_obj.subscription = subscription_detail['name']
+    user_obj.subscription_expiration = date.today() + timedelta(weeks=subscription_detail['duration_months']*4)
+    user_obj.save()
+
+
--- celebMark/payment/views.py
+++ celebMark/payment/views.py
@@ -1,4 +1,4 @@
-from .helpers import create_invite_payment
+from .helpers import create_invite_payment, create_subscription_payment
 from .im_api import im_api
 from .models import Payment
 from django.shortcuts import render
@@ -17,15 +17,18 @@ class IMHookView(APIView):
         post_data = request.POST.copy()
 
         # Collect relevant data from post request
-        pay_data = {}
+        pay_data = {} # Data for Payment Object Creation
         for field in Payment._meta.get_fields():
             if field.name in post_data.keys():
                 pay_data.update({field.name: post_data[field.name]})
         
-        # Check and pass payment to appropriate view method
-        user_obj = BaseUser.objects.get(email=post_data['buyer']).user
-        if post_data['purpose'] == 'invite':
-            create_invite_payment(pay_data, user_obj)
+        pay_data['user'] = BaseUser.objects.get(email=post_data['buyer']).user
+        
+        # Check and pass payment to appropriate method
+        if pay_data['purpose'].startswith('invite'):
+            create_invite_payment(pay_data)
+        elif pay_data['purpose'].startswith('subscribe'):
+            create_subscription_payment(pay_data)
         return Response(status=200)
 
 class CheckPayStatusView(APIView):
@@ -38,21 +41,25 @@ class CheckPayStatusView(APIView):
         im_response = im_api.payment_request_payment_status(
             payment_request_id,
             payment_id
-            )
+        )
         if not im_response['success']:
             return Response({ 'status': 'Failed'})
         
         im_response = im_response['payment_request'].copy()
-        im_response.update(im_response['payment'])
-        im_response['payment_request_id'] = payment_request_id
-        im_response['payment_id'] = payment_id
+        im_response.update(
+            payment_request_id=payment_request_id,
+            **im_response['payment']
+        )
 
-        pay_data = {}
+        pay_data = {} # Data for Payment Object Creation
         for field in Payment._meta.get_fields():
             if field.name in im_response:
                 pay_data.update({field.name: im_response[field.name]})
+        pay_data['user'] = BaseUser.objects.get(email=im_response['buyer_email']).user
 
-        user_obj = BaseUser.objects.get(email=im_response['buyer_email']).user
-        if pay_data['purpose'] == 'invite':
-            create_invite_payment(pay_data, user_obj)
+        # Check and pass payment to appropriate method
+        if pay_data['purpose'].startswith('invite'):
+            create_invite_payment(pay_data)
+        elif pay_data['purpose'].startswith('subscribe'):
+            create_subscription_payment(pay_data)
         return Response({ 'status': pay_data['status'], 'purpose': pay_data['purpose'] })
