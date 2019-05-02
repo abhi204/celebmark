84cce355882f701a673330ac8879a2997f59fcbe
dntandan <deepnarayan006@gmail.com>
2019-05-02 23:25:10 +0530

    celeb card for serach result ui updated

--- frontend/src/App/app_scenes/search/components/celeb_card.js
+++ frontend/src/App/app_scenes/search/components/celeb_card.js
@@ -5,7 +5,7 @@ import {
     MDBCardHeader,
     MDBMedia,
     MDBContainer,
-    MDBRow
+    MDBRow,MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBTooltip, MDBCardFooter,
 } from 'mdbreact';
 import { Link, withRouter } from 'react-router-dom';
 import { API_HOST } from '_consts/api';
@@ -17,55 +17,27 @@ class CelebCard extends Component {
     render(){
         const { name, profile_pic, user_name } = this.props
         return (
-              <MDBRow>
-                  <MDBCol xl="2" lg="2" md="1" sm="1"></MDBCol>
-                  <MDBCol xl="8" lg="8" md="10" sm="10" xs="12">
-                      <MDBContainer fluid className="mt-3 border border-dark rounded">
-                        <MDBCardHeader className="border-0 mt-3 font-weight-bold d-flex justify-content-between">
-                          <p className="mr-4 mb-0">{name}</p>
-                          <ul className="list-unstyled text-default list-inline mb-0">
-                            <li className="list-inline-item mr-3"><a className="cyan-text" href={`/profile/${user_name}`}><MDBIcon className="mr-2" icon="user" />Profile</a></li>
-                          </ul>
-                        </MDBCardHeader>
-                        <MDBMedia className="p-4 bg-white">
-                          <MDBMedia >
-                            <img className="card-img-100 rounded-circle d-flex z-depth-1 mr-3" src={`${largeProfileUrl}`} alt="" />
-                          </MDBMedia>
-                          <MDBMedia body>
-                            <ul className="list-unstyled list-inline mb-2 pt-1 more-padding">
-                            <li className="list-inline-item">
-                              <MDBIcon className="green-text" size="lg" icon="user-circle"/><span className="font-weight-bold"> Artist:</span> {name}
-                              </li>
-                                <br/>
-                              <li className="list-inline-item">
-                                  <MDBIcon  className="green-text" size="lg" icon="poll"/><span className="font-weight-bold"> Type:</span> MUSICIAN//
-                              </li>
-                                <br/>
-                              <li className="list-inline-item">
-                                  <MDBIcon  className="green-text" size="lg" icon="map-marker-alt"/><span className="font-weight-bold"> City:</span> Patna
-                              </li>
-                                <br/>
-                              <li className="list-inline-item pl-0">
-                                <h5>
-                                  <MDBIcon icon="star" className="lime-text pl-2" />
-                                  <MDBIcon icon="star" className="lime-text" />
-                                  <MDBIcon icon="star" className="lime-text" />
-                                  <MDBIcon icon="star" className="lime-text" />
-                                  <MDBIcon far icon="star-half" className="lime-text" />
-                                </h5>
-                              </li>
 
-                            </ul>
-                            <p className="font-weight-bold"></p>
-                          </MDBMedia>
-                        </MDBMedia>
-                      </MDBContainer>
-                  </MDBCol>
-                  <MDBCol xl="2" lg="2" md="1" sm="1"></MDBCol>
-              </MDBRow>
+            <MDBCol >
+              <div className="cards mt-4 ml-4 text-right float-left  ">
+                    <div className="card-left">
+                        <img src="https://laracasts.com/images/series/2018/the-php-practitioner.svg"/>
+                    </div>
+                    <div className="pt-5 card-right">
+                        <a className="black-text " href={`/profile/${user_name}`}>
+                            <h3 className="font-weight-bold card-title">{name}</h3>
+                        </a>
+                        <div className="card-meta">
+                                <span><h5 className="pt-2"> Musician </h5></span>
+                        </div>
 
+                    </div>
+
+                </div>
+            </MDBCol>
         );
     }
 }
 
 export default withRouter(CelebCard);
+
