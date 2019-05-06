359c615bd4ab10f3430d1241d7e28270cc20ec21
Abhi <abhi.chaurasia204@gmail.com>
2019-05-06 19:56:39 +0530

    Update dashboard profile section

--- frontend/src/App/app_scenes/dashboard/scenes/profile/profile.js
+++ frontend/src/App/app_scenes/dashboard/scenes/profile/profile.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import {Link, withRouter} from 'react-router-dom';
-import {MDBRow, MDBCol, MDBCard, MDBCardBody, MDBMask, MDBIcon, MDBView, MDBBtn, MDBContainer} from "mdbreact";
+import { connect } from 'react-redux';
+import {MDBRow, MDBCol, MDBCardBody, MDBMask, MDBIcon, MDBView, MDBBtn, MDBContainer} from "mdbreact";
 import "./profile.css";
 
 class DetailsSection extends Component {
 
     render() {
+        const { user } = this.props;
         return (
             <MDBContainer className="mt-4">
                 <div className="mt-1 px-5 py-3 big-screen-margin-profile">
@@ -18,65 +20,48 @@ class DetailsSection extends Component {
                                         src="https://mdbootstrap.com/img/Photos/Others/img%20(27).jpg"
                                         alt=""
                                     />
-                                    <a href="#!">
                                         <MDBMask overlay="white-slight"/>
-                                    </a>
                                 </MDBView>
                             </MDBCol>
-                            <MDBCol className="pl-4" lg="5">
-                                <a href="#!" className="blue-text">
-                                    <h6 className="font-weight-bold mb-3">
-                                        <MDBIcon icon="info-circle" className="pr-2"/>
-                                        Profile Details
-                                    </h6>
-                                </a>
-                                <h4 className="font-weight-bold mb-2 p-0">
+                            <MDBCol className="pl-4">
+                                <h3 className="font-weight-bold text-center  my-3">
+                                    <MDBIcon icon="info-circle" className="pr-2"/>
+                                    Profile Details &nbsp;
+                                    <sup>
+                                    <Link to="/dashboard/settings"><MDBIcon style={{fontSize: '0.8em'}} far icon="edit" /></Link>
+                                    </sup>
+                                </h3>
+                                <h5 className="font-weight-bold pb-2">
                                     <MDBIcon icon="user-circle" size="md" className="pink-text"/>&nbsp;
-                                    <strong> Deep Narayan Tandan</strong>
-                                </h4>
-                                <h6 className="font-weight-bold mb-2 p-0">
+                                    <strong> {user.first_name} {user.last_name}</strong>
+                                </h5>
+                                <h5 className="font-weight-bold pb-2">
                                     <MDBIcon icon="at" size="lg" className="pink-text"/>&nbsp;&nbsp;
-                                    <strong> deepnarayan006@gmail.com</strong>
-                                </h6>
-                                <h6 className="font-weight-bold mb-2 p-0">&nbsp;
+                                    <strong>{user.email}</strong>
+                                </h5>
+                                <h5 className="font-weight-bold pb-2">&nbsp;
                                     <MDBIcon icon="mobile" size="lg" className="pink-text"/>&nbsp;&nbsp;&nbsp;
-                                    <strong> +91 9982168317</strong>
-                                </h6>
-                                <h6 className="font-weight-bold mb-2 p-0">&nbsp;
-                                    <MDBIcon icon="credit-card" size="md" className="pink-text"/>&nbsp;&nbsp;
-                                    <strong> Free Plan</strong>
-                                </h6>
-
-                                <p>
-                                    <MDBIcon icon="angle-double-right" size="lg" className="pink-text"/>&nbsp;&nbsp;
-                                    Last <strong> updated </strong> on , 19/08/2018
-                                </p>
-                            </MDBCol>
-                            <MDBCol lg="3" className="text-center mt-5">
-                                <Link to='/dashboard/settings'>
-                                    <MDBBtn rounded color="info">
-                                        <MDBIcon icon="pen" className="mr-1"/> <strong>Edit</strong>
-                                    </MDBBtn></Link>
+                                    <strong> +91 {user.mobile}</strong>
+                                </h5>
+                                <h5 className="font-weight-bold pb-2">&nbsp;
+                                    <MDBIcon icon="credit-card" size="md" className="pink-text"/>
+                                    &nbsp;&nbsp;Subscribed to<strong> {user.subscription} Plan</strong>
+                                    &nbsp;<MDBIcon style={{fontSize: '0.7em'}} icon="question-circle" />
+                                </h5>
                             </MDBCol>
 
                         </MDBRow>
                         <hr className="my-1"/>
                         <MDBRow>
-                            <MDBCardBody cascade className="text-center my-4">
-                                <h2 className="font-weight-bold">
+                            <MDBCardBody cascade className="text-center">
+                                <h2 className="font-weight-bold mb-3">
                                     <MDBIcon icon="layer-group" size="md" className="pink-text pr-2"/>
-                                    <strong> Statistics</strong>
+                                    <strong> Summary</strong>
                                 </h2>
-                                <h5 className="pt-1 pb-2">
-                                    Updated On
-                                    <a href="#!">
-                                        <strong> 24/05/2019</strong>
-                                    </a>
-                                </h5>
                                 <Link to="/dashboard/invites">
                                     <MDBBtn rounded className="btn-fb waves-light">
                                         <MDBIcon icon="gift" className="pr-2"/>
-                                        <strong>Invite Sent</strong>
+                                        <strong>Invites Sent</strong>
                                     </MDBBtn>
                                     <span className="counter">46</span>
                                 </Link>
@@ -101,113 +86,6 @@ class DetailsSection extends Component {
                                 </Link>
                             </MDBCardBody>
                         </MDBRow>
-                        <hr className="my-1"/>
-                        <MDBRow>
-                            <MDBContainer>
-                                <section className="my-3">
-                                    <h2 className="h1-responsive font-weight-bold text-center my-5">
-                                        <MDBIcon icon="feather-alt" size="md" className="blue-text mr-2" rotate="90"
-                                                 flip="horizontal"/>
-                                        Features of <strong>Free</strong> plan !
-                                    </h2>
-                                    <MDBRow>
-                                        <MDBCol md="4" className="md-0 mb-5">
-                                            <MDBRow>
-                                                <MDBCol lg="2" md="3" size="2">
-                                                    <MDBIcon icon="bullhorn" size="2x" className="blue-text"/>
-                                                </MDBCol>
-                                                <MDBCol lg="10" md="9" size="10">
-                                                    <h4 className="font-weight-bold">Invites</h4>
-                                                    <p className="grey-text">
-                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                                                        do eiusmod tempor incididunt ut labore et dolore magna
-                                                        aliqua. Ut enim ad minim veniam.
-                                                    </p>
-                                                </MDBCol>
-                                            </MDBRow>
-                                        </MDBCol>
-                                        <MDBCol md="4" className="md-0 mb-5">
-                                            <MDBRow>
-                                                <MDBCol lg="2" md="3" size="2">
-                                                    <MDBIcon icon="clock" size="2x" className="pink-text"/>
-                                                </MDBCol>
-                                                <MDBCol lg="10" md="9" size="10">
-                                                    <h4 className="font-weight-bold">Validity</h4>
-                                                    <p className="grey-text">
-                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                                                        do eiusmod tempor incididunt ut labore et dolore magna
-                                                        aliqua. Ut enim ad minim veniam.
-                                                    </p>
-                                                </MDBCol>
-                                            </MDBRow>
-                                        </MDBCol>
-                                        <MDBCol md="4" className="md-0 mb-5">
-                                            <MDBRow>
-                                                <MDBCol lg="2" md="3" size="2">
-                                                    <MDBIcon icon="headset" size="2x" className="purple-text"/>
-                                                </MDBCol>
-                                                <MDBCol lg="10" md="9" size="10">
-                                                    <h4 className="font-weight-bold">Support</h4>
-                                                    <p className="grey-text">
-                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                                                        do eiusmod tempor incididunt ut labore et dolore magna
-                                                        aliqua. Ut enim ad minim veniam.
-                                                    </p>
-                                                </MDBCol>
-                                            </MDBRow>
-                                        </MDBCol>
-                                    </MDBRow>
-                                    <MDBRow>
-                                        <MDBCol md="4" className="md-0 mb-5">
-                                            <MDBRow>
-                                                <MDBCol lg="2" md="3" size="2">
-                                                    <MDBIcon icon="glass-martini-alt" size="2x" className="blue-text"/>
-                                                </MDBCol>
-                                                <MDBCol lg="10" md="9" size="10">
-                                                    <h4 className="font-weight-bold">Post Invites</h4>
-                                                    <p className="grey-text">
-                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                                                        do eiusmod tempor incididunt ut labore et dolore magna
-                                                        aliqua. Ut enim ad minim veniam.
-                                                    </p>
-                                                </MDBCol>
-                                            </MDBRow>
-                                        </MDBCol>
-                                        <MDBCol md="4" className="md-0 mb-5">
-                                            <MDBRow>
-                                                <MDBCol lg="2" md="3" size="2">
-                                                    <MDBIcon icon="credit-card" size="2x" className="pink-text"/>
-                                                </MDBCol>
-                                                <MDBCol lg="10" md="9" size="10">
-                                                    <h4 className="font-weight-bold">Refund</h4>
-                                                    <p className="grey-text">
-                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                                                        do eiusmod tempor incididunt ut labore et dolore magna
-                                                        aliqua. Ut enim ad minim veniam.
-                                                    </p>
-                                                </MDBCol>
-                                            </MDBRow>
-                                        </MDBCol>
-                                        <MDBCol md="4" className="md-0 mb-5">
-                                            <MDBRow>
-                                                <MDBCol lg="2" md="3" size="2">
-                                                    <MDBIcon icon="tachometer-alt" size="2x" className="purple-text"/>
-                                                </MDBCol>
-                                                <MDBCol lg="10" md="9" size="10">
-                                                    <h4 className="font-weight-bold">Priority</h4>
-                                                    <p className="grey-text">
-                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                                                        do eiusmod tempor incididunt ut labore et dolore magna
-                                                        aliqua. Ut enim ad minim veniam.
-                                                    </p>
-                                                </MDBCol>
-                                            </MDBRow>
-                                        </MDBCol>
-                                    </MDBRow>
-                                </section>
-
-                            </MDBContainer>
-                        </MDBRow>
                     </MDBCardBody>
                 </div>
             </MDBContainer>
@@ -215,5 +93,9 @@ class DetailsSection extends Component {
     }
 }
 
-export default withRouter(DetailsSection);
+let mapStateTopProps = (state) => ({
+    user: state.user.details
+})
+
+export default withRouter(connect(mapStateTopProps)(DetailsSection));
 
