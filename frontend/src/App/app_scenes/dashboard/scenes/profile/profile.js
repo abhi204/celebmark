f701973f3a979aae55c6a6733b6c92f967c141d6
dntandan <deepnarayan006@gmail.com>
2019-05-06 19:46:19 +0530

    dashboard profile content updated

--- frontend/src/App/app_scenes/dashboard/scenes/profile/profile.js
+++ frontend/src/App/app_scenes/dashboard/scenes/profile/profile.js
@@ -11,7 +11,7 @@ class DetailsSection extends Component {
                 <div className="mt-1 px-5 py-3 big-screen-margin-profile">
                     <MDBCardBody>
                         <MDBRow className="pt-3 mb-3 ">
-                            <MDBCol lg="3">
+                            <MDBCol lg="3" >
                                 <MDBView className="rounded z-depth-2 mb-lg-0 mb-4" hover waves>
                                     <img
                                         className="img-fluid"
@@ -23,20 +23,20 @@ class DetailsSection extends Component {
                                     </a>
                                 </MDBView>
                             </MDBCol>
-                            <MDBCol className="pl-4" lg="5">
+                            <MDBCol className="pl-4 mobile-content-profile" lg="5" >
                                 <a href="#!" className="blue-text">
                                     <h6 className="font-weight-bold mb-3">
                                         <MDBIcon icon="info-circle" className="pr-2"/>
                                         Profile Details
                                     </h6>
                                 </a>
-                                <h4 className="font-weight-bold mb-2 p-0">
+                                <h4 className="h4-responsive font-weight-bold mb-2 p-0">
                                     <MDBIcon icon="user-circle" size="md" className="pink-text"/>&nbsp;
-                                    <strong> Deep Narayan Tandan</strong>
+                                    <strong> Abhi Chaurasia </strong>
                                 </h4>
                                 <h6 className="font-weight-bold mb-2 p-0">
                                     <MDBIcon icon="at" size="lg" className="pink-text"/>&nbsp;&nbsp;
-                                    <strong> deepnarayan006@gmail.com</strong>
+                                    <strong> testemail@gmail.com</strong>
                                 </h6>
                                 <h6 className="font-weight-bold mb-2 p-0">&nbsp;
                                     <MDBIcon icon="mobile" size="lg" className="pink-text"/>&nbsp;&nbsp;&nbsp;
@@ -65,7 +65,7 @@ class DetailsSection extends Component {
                             <MDBCardBody cascade className="text-center my-4">
                                 <h2 className="font-weight-bold">
                                     <MDBIcon icon="layer-group" size="md" className="pink-text pr-2"/>
-                                    <strong> Statistics</strong>
+                                    <strong> Invites</strong>
                                 </h2>
                                 <h5 className="pt-1 pb-2">
                                     Updated On
@@ -76,26 +76,26 @@ class DetailsSection extends Component {
                                 <Link to="/dashboard/invites">
                                     <MDBBtn rounded className="btn-fb waves-light">
                                         <MDBIcon icon="gift" className="pr-2"/>
-                                        <strong>Invite Sent</strong>
+                                        <strong>Sent</strong>
                                     </MDBBtn>
                                     <span className="counter">46</span>
                                 </Link>
                                 <MDBBtn rounded className="btn-tw waves-light">
                                     <MDBIcon far icon="bell" className="pr-2"/>
-                                    <strong> Invite Remaining</strong>
+                                    <strong>Remaining</strong>
                                 </MDBBtn>
                                 <span className="counter">22</span>
                                 <Link to="/dashboard/invites">
                                     <MDBBtn rounded className=" waves-light">
                                         <MDBIcon icon="comments" className="pr-2"/>
-                                        <strong>Invite Accepeted</strong>
+                                        <strong>Accepeted</strong>
                                     </MDBBtn>
                                 </Link>
                                 <span className="counter">31</span>
                                 <Link to="/dashboard/invites">
                                     <MDBBtn rounded className="btn-gplus waves-light">
                                         <MDBIcon icon="ban" className="pr-2"/>
-                                        <strong>Invite Cancelled</strong>
+                                        <strong>Cancelled</strong>
                                     </MDBBtn>
                                     <span className="counter">18</span>
                                 </Link>
