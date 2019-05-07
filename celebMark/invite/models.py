c0654e4f4e63a9881b04070f3d8aa703e4322bfe
Abhi <abhi.chaurasia204@gmail.com>
2019-05-07 16:21:43 +0530

    Add status field to invite model

--- celebMark/invite/models.py
+++ celebMark/invite/models.py
@@ -4,15 +4,22 @@ import uuid
 
 # Create your models here.
 class Invite(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'pending'),
+        ('accepted', 'accepted'),
+        ('cancelled', 'cancelled')
+    )
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     celeb = models.ForeignKey(Celeb, on_delete=models.SET_NULL, null=True)
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
-    event_type = models.CharField(max_length=100, default="College Event")
+    event_type = models.CharField(max_length=100, default='College Event')
     role = models.CharField(max_length=100)
-    city = models.CharField(max_length=100, default="MUMBAI")
+    city = models.CharField(max_length=100, default='MUMBAI')
     event_date = models.DateField()
     venue = models.TextField()
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='pending')
     description = models.TextField(blank=True)
     payment_request_id = models.TextField(default="")
 
