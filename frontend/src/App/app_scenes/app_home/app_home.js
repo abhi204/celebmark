53675f7586a5ad7eaa93a7a2185d654eb09fc21e
Abhi <abhi.chaurasia204@gmail.com>
2019-05-10 19:16:04 +0530

    Remove Sponser slider from homepage

--- frontend/src/App/app_scenes/app_home/app_home.js
+++ frontend/src/App/app_scenes/app_home/app_home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Carousel from '_components/carousel/carousel';
-import SponsorSlider from '_components/sponsor_list/sponsor_list';
 import SubscriptionCards from './containers/subscription_cards/subscription_cards'
 import { MDBContainer } from 'mdbreact';
 
@@ -13,7 +12,7 @@ class AppHome extends Component {
                 <Carousel/>
                 <br/>
                 <MDBContainer>
-                    <SponsorSlider/>
+                    {/* <SponsorSlider/> */}
                     <center><SubscriptionCards/></center>
                 </MDBContainer>
             </div>
