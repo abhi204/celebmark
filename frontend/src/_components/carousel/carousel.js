a7faf2cc95cdeaeb0c5478c3afe48a0bd3923773
Abhi <abhi.chaurasia204@gmail.com>
2019-05-10 19:16:41 +0530

    Create Loader for carousel on homepage

--- frontend/src/_components/carousel/carousel.js
+++ frontend/src/_components/carousel/carousel.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { 
     MDBCarousel, MDBCarouselCaption,
     MDBCarouselInner, MDBCarouselItem,
-    MDBView, MDBMask 
+    MDBView, MDBMask, MDBSpinner 
 } from 'mdbreact';
 import { imageFetcher } from './helper/image_fetcher';
 import './carousel.css';
@@ -10,19 +10,31 @@ import './carousel.css';
 export default class Carousel extends Component {
   constructor(props){
     super(props);
-    this.state = {numberOfImages: 3, images: [] }
+    this.state = {numberOfImages: 3, images: [], loading: true }
   }
 
   async componentDidMount(){
     let images = await imageFetcher(this.state.numberOfImages);
-    this.setState({images})
+    this.setState({images, loading: false})
   }
 
   render(){
-    const { numberOfImages, images } = this.state;
+    const { numberOfImages, images, loading } = this.state;
+    if(loading){
+      return (
+        <div 
+          className="carousel-image carousel-loading
+          d-flex flex-column justify-content-center align-content-center"
+        >
+          <center><MDBSpinner red/></center>
+      </div>
+      )
+    }
     return (
       <div >
-        <MDBCarousel activeItem={1} length={numberOfImages} showControls={true} showIndicators={true} className="z-depth-1 d-none d-sm-block">
+        <MDBCarousel activeItem={1} length={numberOfImages} showControls={false} 
+          showIndicators={false} className="z-depth-1 carousel-image"
+        >
           <MDBCarouselInner>
             { images.map(image => (
               <MDBCarouselItem itemId={image.id} key={image.id}>
@@ -31,26 +43,7 @@ export default class Carousel extends Component {
                   <MDBMask overlay="black-slight" />
                 </MDBView>
                 <MDBCarouselCaption>
-                  <h3 className="h3-responsive">This is caption</h3>
-                  <p>Second text</p>
-                </MDBCarouselCaption>
-              </MDBCarouselItem>
-              ))}
-          </MDBCarouselInner>
-        </MDBCarousel>
-
-         {/* Carousel for Mobile Devices */}
-         <MDBCarousel activeItem={1} length={numberOfImages} showControls={true} showIndicators={true} className="z-depth-1 d-block d-sm-none">
-          <MDBCarouselInner>
-            { images.map(image => (
-              <MDBCarouselItem itemId={image.id} key={image.id}>
-                <MDBView className="carousel-image-sm">
-                  <img className="d-block w-100" src={image.url} alt="Second slide" />
-                  <MDBMask overlay="black-slight" />
-                </MDBView>
-                <MDBCarouselCaption>
-                  <h3 className="h3-responsive">This is caption</h3>
-                  <p>XYZ</p>
+                  <h3 className="h3-responsive">Get people For your event</h3>
                 </MDBCarouselCaption>
               </MDBCarouselItem>
               ))}
